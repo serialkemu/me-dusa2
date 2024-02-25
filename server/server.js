1c6a4623c860285626ccb70e716b31e4bd411b80
Justus Ombati <84173094+romanticgangsta@users.noreply.github.com>
2024-02-25 15:28:53 +0300

    Update server.js
    deleted line 3 4

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const authRouter = require('./Routes/authRouter');
-// const userRouter = require('./Routes/userRouter');
+
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
